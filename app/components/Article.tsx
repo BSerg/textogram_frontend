9cf54cba147f46ea68eed2618c1f25656b106640
Sergei Bychkov <bychkov.sergei.vladimirovich@gmail.com>
2017-02-03 16:20:06 +0300

    update

--- app/components/Article.tsx
+++ app/components/Article.tsx
@@ -174,7 +174,7 @@ export default class Article extends React.Component<any, IArticleState> {
                             </div> : null
                         }
                     </div>
-                    : <div className="article__loading"><span>ARTICLE</span> IS LOADING...</div>
+                    : <div className="article__loading"><span>СТАТЬЯ</span> ЗАГРУЖАЕТСЯ...</div>
                 : this.state.error
         )
     }
