6641783b9b7dfb4617a822985e3985bc60f74ae3
Sergei Bychkov <bychkov.sergei.vladimirovich@gmail.com>
2017-04-11 14:04:37 +0300

    fix editor phrase

--- app/components/editor/PhraseContentBlock.tsx
+++ app/components/editor/PhraseContentBlock.tsx
@@ -20,9 +20,12 @@ interface IPhraseContentBlockProps {
     className?: string
 }
 
+type contentLengthState = 'short' | 'normal' | 'long';
+
 interface IPhraseContentBlockState {
-    content?: IPhraseContent
-    isValid?: boolean
+    content?: IPhraseContent;
+    isValid?: boolean;
+    contentLengthState?: contentLengthState;
 }
 
 export default class PhraseContentBlock extends React.Component<IPhraseContentBlockProps, IPhraseContentBlockState> {
@@ -30,12 +33,23 @@ export default class PhraseContentBlock extends React.Component<IPhraseContentBl
         super(props);
         this.state = {
             content: this.props.content as IPhraseContent,
-            isValid: this.isValid(this.props.content as IPhraseContent)
+            isValid: this.isValid(this.props.content as IPhraseContent),
+            contentLengthState: this.getContentLengthState((this.props.content as IPhraseContent).value)
         }
     }
 
     private isValid(content: IPhraseContent): boolean {
         return Validator.isValid(content, Validation.PHRASE);
+    }
+
+    private getContentLengthState(value: string) {
+        if (value.length <= 70) {
+            return 'short';
+        } else if (value.length <= 200) {
+            return 'normal';
+        } else {
+            return 'long';
+        }
     }
 
     handleFocus() {
@@ -49,7 +63,11 @@ export default class PhraseContentBlock extends React.Component<IPhraseContentBl
             NotificationAction.do(SHOW_NOTIFICATION, {content: Validation.PHRASE.value.message});
         }
         this.state.content.__meta = {is_valid: isValid};
-        this.setState({content: this.state.content, isValid: isValid}, () => {
+        this.setState({
+            content: this.state.content,
+            isValid: isValid,
+            contentLengthState: this.getContentLengthState(contentText)
+        }, () => {
             ContentAction.do(UPDATE_CONTENT_BLCK, {contentBlock: this.state.content});
         });
     }
@@ -59,6 +77,9 @@ export default class PhraseContentBlock extends React.Component<IPhraseContentBl
         if (this.props.className) {
             className += ' ' + this.props.className;
         }
+        if (this.state.contentLengthState != 'normal') {
+            className += ' ' + this.state.contentLengthState;
+        }
         !this.state.isValid && (className += ' invalid');
         return (
             <BaseContentBlock id={this.props.content.id} className={className}>
@@ -67,6 +88,7 @@ export default class PhraseContentBlock extends React.Component<IPhraseContentBl
                                  onFocus={this.handleFocus.bind(this)}
                                  onChange={this.handleChange.bind(this)}
                                  onChangeDelay={0}
+                                 alignContent="center"
                                  content={this.state.content.value}
                                  placeholder={Captions.editor.enter_phrase}/>
             </BaseContentBlock>
