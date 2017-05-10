37a1567a2dd6f11ee492a38d26637d1c254b58de
Sergei Bychkov <bychkov.sergei.vladimirovich@gmail.com>
2017-05-10 12:39:56 +0300

    fix require

--- app/index.d.ts
+++ app/index.d.ts
@@ -1,9 +1,3 @@
-declare var require: {
-    <T>(path: string): T;
-    (paths: string[], callback: (...modules: any[]) => void): void;
-    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
-};
-
 interface IToMarkdown {
     (input: string, options?: any): string
 }
