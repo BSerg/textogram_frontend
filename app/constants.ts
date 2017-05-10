241e4cf0b37f862879a434a50765b292ede45f41
Sergei Bychkov <bychkov.sergei.vladimirovich@gmail.com>
2017-05-10 16:32:45 +0300

    update banners

--- app/constants.ts
+++ app/constants.ts
@@ -326,6 +326,7 @@ export namespace Validation {
 
 export const BannerID = {
     BANNER_TOP: 'banner__top',
+    BANNER_BOTTOM: 'banner__bottom',
     BANNER_CONTENT: 'banner__content',
     BANNER_RIGHT_SIDE: 'banner__right_side',
     BANNER_CONTENT_INLINE: 'banner__content_inline'
