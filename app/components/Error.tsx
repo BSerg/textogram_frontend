e013c8f147bff68596cc7192f9d8121eda3389a2
Sergei Bychkov <bychkov.sergei.vladimirovich@gmail.com>
2017-05-22 11:08:08 +0300

    fix error pages

--- app/components/Error.tsx
+++ app/components/Error.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+
 import '../styles/error.scss';
 
 interface ErrorProps {
@@ -31,12 +32,14 @@ export class Error404 extends React.Component<any, any> {
     }
 
     render() {
+        let className = process.env.IS_LENTACH ? 'error_lentach' : 'error';
+
         return (
-            <div className="error error404">
+            <div className={className + " error404"}>
                 <div className="error__wrapper">
                     <div className="error__image"></div>
                     <div className="error__content">
-                        <div className="error__code"></div>
+                        <div className="error__code">{!process.env.IS_LENTACH ? 404 : null}</div>
                         <div className="error__msg">Страница не найдена</div>
                     </div>
                 </div>
@@ -51,16 +54,19 @@ export class Error403 extends React.Component<any, any> {
     }
 
     render() {
+        let className = process.env.IS_LENTACH ? 'error_lentach' : 'error';
         return (
-            <div className="error error403">
+            <div className={className + " error403"}>
                 <div className="error__wrapper">
                     <div className="error__image"></div>
                     <div className="error__content">
-                        <div className="error__code"></div>
-                        <div className="error__msg">
-                            Кажется, у вас нет доступа<br/>
-                            к этой странице, сорян
-                        </div>
+                        <div className="error__code">{!process.env.IS_LENTACH ? 403 : null}</div>
+                        {process.env.IS_LENTACH ?
+                            <div className="error__msg">
+                                Кажется, у вас нет доступа<br/>
+                                к этой странице, сорян
+                            </div> : <div>У вас нет доступа к этой странице.</div>
+                        }
                     </div>
                 </div>
             </div>
@@ -74,17 +80,20 @@ export class Error500 extends React.Component<any, any> {
     }
 
     render() {
+        let className = process.env.IS_LENTACH ? 'error_lentach' : 'error';
         return (
-            <div className="error error500">
+            <div className={className + " error500"}>
                 <div className="error__wrapper">
                     <div className="error__image"></div>
                     <div className="error__content">
-                        <div className="error__code"></div>
-                        <div className="error__msg">
-                            Мы все исправим, а пока держитесь<br/>
-                            здесь, всего вам доброго, хорошего<br/>
-                            настроения и здоровья
-                        </div>
+                        <div className="error__code">{!process.env.IS_LENTACH ? 500 : null}</div>
+                        {process.env.IS_LENTACH ?
+                            <div className="error__msg">
+                                Мы все исправим, а пока держитесь<br/>
+                                здесь, всего вам доброго, хорошего<br/>
+                                настроения и здоровья
+                            </div> : <div>Произошла ошибка. Очень скоро мы решим эту проблему.</div>
+                        }
                     </div>
                 </div>
             </div>
