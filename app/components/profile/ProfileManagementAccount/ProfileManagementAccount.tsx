b94fd2b2831486759d78b1abb3e3e67e9ca7a44b
mihailkuznetsov <mihasiksli@yandex.ru>
2017-10-08 23:12:10 +0300

    social accs started

--- app/components/profile/ProfileManagementAccount/ProfileManagementAccount.tsx
+++ app/components/profile/ProfileManagementAccount/ProfileManagementAccount.tsx
@@ -2,12 +2,40 @@ import * as React from 'react';
 import {connect} from 'react-redux';
 import Nickname from './ProfilemanagementAccountNickname';
 
-
-
-export function ProfileAccount() {
-    return <div>
-        <Nickname />
+export function AuthAccount(props: {socialLink: any}) {
+    return <div className="profile_management_links">
+        <div className="title">
+            Аккаунт авторизации:
+        </div>
+        <div className="main">
+            
+        </div>
     </div>;
 }
 
-export default ProfileAccount;
+export function ProfileAccount(props: any) {
+    let {socialLink} = props;
+    console.log(socialLink);
+    return <div>
+        <Nickname />
+        {socialLink && <AuthAccount socialLink={socialLink}/>}
+    </div>;
+}
+
+const mapStateToProps = (state: any) => {
+    let socialLinks: any[] = state.userData.user.social_links;
+    let socialLink = null;
+    
+    try {
+        socialLink = socialLinks.find((val: any, index: number) => {
+            return val.is_auth == true;
+        }) || null;
+    } catch( error ) {
+        socialLink = null
+    }
+    return {
+        socialLink
+    }
+}
+
+export default connect(mapStateToProps, null)(ProfileAccount);
